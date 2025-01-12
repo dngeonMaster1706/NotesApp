fdbcff71cf7dfdeca5b62e03463dc6a8c8797905
dngeonMasteer <kaushikborah4080@gmail.com>
2025-01-12 16:09:52 +0530

    Note Card

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md'
 const Home = () => {
+
   return (
-    <div>
-      Home
-    </div>
+    <>
+        <Navbar/>
+        <div className='container mx-auto'>
+        <div className='grid grid-cols-3 gap-4 mt-8'>
+        <NoteCard
+          title="Meeeting on 6"
+          date = "1st Jan"
+          content = "It is about the upcoming session on "
+          tags="#meeting"
+          isPinned={true}
+          onEdit={()=>{}}
+          onDelete={()=>{}}
+          onPinNote={()=>{}}
+        />
+        </div>
+      </div>
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' onClick={()=>{}}>
+        <MdAdd className="text-[32px] text-white"/>
+
+      </button>
+    </>
+
   )
 }
 
